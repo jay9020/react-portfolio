b4ee0ae85a5ee964dfeb41061e85f25199872221
jamie247 <jamie.allen@bbc.co.uk>
2021-03-16 22:24:51 +0000

    Modified App.js to show blue background

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
+     <div style={{backgroundColor: 'blue',}}/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
