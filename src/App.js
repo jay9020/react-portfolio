96f3f5e4125dc0cb7df9ff1e9dbf31f108d7bc89
jamie247 <jamie.allen@bbc.co.uk>
2021-03-16 23:49:20 +0000

    Modified index.html to include newer fonts

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ function App() {
     <div className="App"> 
         <header className="App-header">
         <img class="circle" src="https://styles.redditmedia.com/t5_2txf4/styles/communityIcon_9tb7fpieqvz51.png?width=256&s=e4475878d6b9e8aaa43e1d3f613e74a9c3806954" height="200px" width="200px" />
-        <p style="font-family:Montserrat;";>
+        <h2>
 	  Hi, I'm Jamie Allen, a DevOps and Platform Engineer based in the UK
+	</h2>
+	<p>
+	  If you would like to speak to me, please get in touch
         </p>
       </header>
     </div>
