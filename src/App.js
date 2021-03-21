137ed2404530cf514c78ba8732116424343f8101
jamie247 <jamie.allen@bbc.co.uk>
2021-03-21 13:34:40 +0000

    Modified App.js to add another image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App"> 
         <header className="App-header">
         <img class="circle" src="https://styles.redditmedia.com/t5_2txf4/styles/communityIcon_9tb7fpieqvz51.png?width=256&s=e4475878d6b9e8aaa43e1d3f613e74a9c3806954" height="200px" width="200px" />
-        <h2>
+        <img class="circle" src="https://assets.vg247.com/current/2016/06/dead_rising_41-600x338.jpg" height="200px" width="200px" />
+	  <h2>
 	  Hi, I'm Jamie Allen, a DevOps and Platform Engineer based in the UK
 	</h2>
 	<p>
