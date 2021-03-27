917556639365b70e666fe7639e5a1460a8deda17
jamie247 <jamie.allen@bbc.co.uk>
2021-03-27 19:53:55 +0000

    Modified App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Image from 'react-bootstrap/Image';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import './App.css'
 
 function App() {
   return (
-    <div className="App"> 
-        <header className="App-header">
+    <div className="App">
 
-	<div className="image-row">
-	    <div className="image-column">
-	      <img className="circle" src="https://styles.redditmedia.com/t5_2txf4/styles/communityIcon_9tb7fpieqvz51.png?width=256&s=e4475878d6b9e8aaa43e1d3f613e74a9c3806954" alt="Lost Planet" height="200px" width="200px" />
-	    </div>
-	    <div className="image-column">
-	      <img className="circle" src="https://styles.redditmedia.com/t5_2txf4/styles/communityIcon_9tb7fpieqvz51.png?width=256&s=e4475878d6b9e8aaa43e1d3f613e74a9c3806954" alt="Dead Rising" height="200px" width="200px" />
-	    </div>
+	  <Navbar collapseOnSelect expand="lg">
+	  	<Navbar.Brand href="/">Jamie Allen</Navbar.Brand>
+	  	<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+	  	<Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+	  	<Nav className="justify-content-end" style={{ width: "100%" }}>
+	  		<Nav.Link href="#features" style={{ color: "#fff" }}>About</Nav.Link>
+	  		<Nav.Link href="#pricing" style={{ color: "#fff" }} >Projects</Nav.Link>
+	  		</Nav>
+	  		</Navbar.Collapse>
+	  	</Navbar>
+
+	  <div className="site-container">
+          
+	 	 <div className="header-section">
+
+	  	<Image src="https://styles.redditmedia.com/t5_2txf4/styles/communityIcon_9tb7fpieqvz51.png?width=256&s=e4475878d6b9e8aaa43e1d3f613e74a9c3806954" roundedCircle />
+	  	<p></p>
+	  	<p></p>
+
+	  	<h2 style={{textAlign: 'center'}}>
+	  	Hi, I'm Jamie Allen, a DevOps and Platform Engineer based in the UK
+	  	</h2>
+	
+	  	<p style={{textAlign: 'center'}}>
+	  	If you would like to speak to me, please get in touch
+          	</p>
+
+	  	</div>
+
+		<div className="section">
+	        
+	  	</div>
+
 	  </div>
 
-	  <h2>
-	  Hi, I'm Jamie Allen, a DevOps and Platform Engineer based in the UK
-	  </h2>
-	
-	  <p>
-	  If you would like to speak to me, please get in touch
-          </p>
-         </header>
-    </div>
+     </div>
   );
 }
 
